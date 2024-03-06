4929f41abffcbe222e738e5160d6f8ef2a45b7c5
RIG360 <office@rimpfl.at>
2024-03-06 14:13:46 +0100

    upload

---
+++ data.js
@@ -0,0 +1,361 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-heiz-r-1",
+      "name": "Heiz-R (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -3.1147153520939668,
+        "pitch": -0.010498322139879335,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.017233311451484,
+          "pitch": 0.7103217626671938,
+          "rotation": 0,
+          "target": "1-heiz-r-2"
+        },
+        {
+          "yaw": 2.793260428905535,
+          "pitch": 0.4717351837888586,
+          "rotation": 0,
+          "target": "2-heiz-r-3"
+        },
+        {
+          "yaw": 1.9457441661035384,
+          "pitch": 0.6714157575032225,
+          "rotation": 0,
+          "target": "6-heiz-r-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-heiz-r-2",
+      "name": "Heiz-R (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -2.9547662075717938,
+        "pitch": -0.013996053561626809,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09275714073772612,
+          "pitch": 0.9166474002935736,
+          "rotation": 0,
+          "target": "0-heiz-r-1"
+        },
+        {
+          "yaw": 2.654525172493371,
+          "pitch": 0.6702413835098859,
+          "rotation": 0,
+          "target": "2-heiz-r-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-heiz-r-3",
+      "name": "Heiz-R (3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 2.649123895092945,
+        "pitch": -0.03738145785739988,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5140848215153699,
+          "pitch": 0.9502915828635263,
+          "rotation": 0,
+          "target": "1-heiz-r-2"
+        },
+        {
+          "yaw": -0.29304565074981426,
+          "pitch": 0.611233706648461,
+          "rotation": 0,
+          "target": "0-heiz-r-1"
+        },
+        {
+          "yaw": 1.8474640927153345,
+          "pitch": 0.5971911553063407,
+          "rotation": 0,
+          "target": "3-heiz-r-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-heiz-r-4",
+      "name": "Heiz-R (4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 1.7516762516123023,
+        "pitch": 0.04486315024892207,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.454170067170237,
+          "pitch": 0.6661247387778744,
+          "rotation": 0,
+          "target": "2-heiz-r-3"
+        },
+        {
+          "yaw": -1.1705898381740845,
+          "pitch": 0.468060736095671,
+          "rotation": 0.7853981633974483,
+          "target": "1-heiz-r-2"
+        },
+        {
+          "yaw": 1.5529391801075736,
+          "pitch": 0.6566115841978117,
+          "rotation": 0,
+          "target": "4-heiz-r-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-heiz-r-5",
+      "name": "Heiz-R (5)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 1.4872799709161386,
+        "pitch": -0.03934333856908623,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7606864393980892,
+          "pitch": 0.6579594681898264,
+          "rotation": 0,
+          "target": "3-heiz-r-4"
+        },
+        {
+          "yaw": -1.6171561805221781,
+          "pitch": 0.40515188558628523,
+          "rotation": 0,
+          "target": "2-heiz-r-3"
+        },
+        {
+          "yaw": 0.004190956045087901,
+          "pitch": 0.6279398679317865,
+          "rotation": 0,
+          "target": "5-heiz-r-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-heiz-r-6",
+      "name": "Heiz-R (6)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.007671303718232991,
+        "pitch": -0.009213764269834002,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0936144315699323,
+          "pitch": 0.6124944795467133,
+          "rotation": 0,
+          "target": "4-heiz-r-5"
+        },
+        {
+          "yaw": -1.4878343676420425,
+          "pitch": 0.4607709943275182,
+          "rotation": 0,
+          "target": "6-heiz-r-7"
+        },
+        {
+          "yaw": -1.4350544552610192,
+          "pitch": 0.310276653007886,
+          "rotation": 0,
+          "target": "0-heiz-r-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-heiz-r-7",
+      "name": "Heiz-R (7)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -1.3735415916051181,
+        "pitch": -0.031155248227761945,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6996721338369003,
+          "pitch": 0.48565838905513203,
+          "rotation": 0,
+          "target": "5-heiz-r-6"
+        },
+        {
+          "yaw": -1.1532859192460059,
+          "pitch": 0.6711952277959874,
+          "rotation": 0,
+          "target": "0-heiz-r-1"
+        },
+        {
+          "yaw": -1.894324757617765,
+          "pitch": 0.6678237303901664,
+          "rotation": 0,
+          "target": "1-heiz-r-2"
+        },
+        {
+          "yaw": -2.376365019109283,
+          "pitch": 0.5806039951670403,
+          "rotation": 0,
+          "target": "2-heiz-r-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mill34, Heiz-R_240305",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
